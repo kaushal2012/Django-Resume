10c0e00d68b073ff9df1ca2e74dfb7c18c7ee62c
kaushal2012 <kaushalsinhvadher@gmail.com>
2024-01-02 18:44:00 +0530

    commit

--- myresume/core/views.py
+++ myresume/core/views.py
@@ -4,6 +4,3 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'core/home.html')
-
-def Services(request):
-    return render(request, 'core/services.html')
