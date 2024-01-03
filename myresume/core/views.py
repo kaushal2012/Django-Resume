b47021c4c5cd96c5659752e03249138e2276452d
kaushal2012 <kaushalsinhvadher@gmail.com>
2024-01-03 18:36:51 +0530

    commit

--- myresume/core/views.py
+++ myresume/core/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'core/home.html')
+
+
--- myresume/myresume/urls.py
+++ myresume/myresume/urls.py
@@ -21,5 +21,7 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('serv/', include('serv.urls')),
+    path('edu/', include('edu.urls')),
 
 ]
